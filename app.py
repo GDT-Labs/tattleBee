4227fa3ab4d8c77a6b0fd2b9d9bc49c2cb6ae1c2
Andrew Johnson <andrew.johnson@gdt.com>
2016-07-09 23:39:42 -0500

    publish capture

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import paho.mqtt.client as mqtt
 import json
+from imgurpython import ImgurClient
+import os
+import time
 
 client_id = '75f55a5c2221a79'
 client_secret = '7b039d13d0556893c3e5bc6461463ca5f22f3ee3'
@@ -23,10 +26,16 @@ def get_image_and_post():
     client = ImgurClient(client_id, client_secret)
     os.system('raspistill -vf -hf -o /home/pi/camera/pic.jpg')
     capture = client.upload_from_path('/home/pi/camera/pic.jpg', config=None, anon=True)
+    publish_URL(capture['link'])
     print "Uploaded image to " + capture['link']
 
 def publish_URL(url):
-    return NotImplemented
+    message2 = {
+    'meaning': 'image',
+    'value': url
+    }
+    client.publish(credentials['topic'] +'data', json.dumps(message2))
+
 
 mqttc = mqtt.Client()
 mqttc.username_pw_set('9fd83945-7910-409f-ac4d-ff79128f3fec', 'sIrfSbTIfjHm')
@@ -41,5 +50,3 @@ mqttc.subscribe("/v1/9fd83945-7910-409f-ac4d-ff79128f3fec/data", qos=0)
 
 while (True):
     mqttc.loop()
-    get_image_and_post():
-    time.sleep(10)
